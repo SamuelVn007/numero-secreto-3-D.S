64682eb9c4e5b9af921701bc52b8c29ee5356a87
SamuelVn007 <134400857+SamuelVn007@users.noreply.github.com>
2024-09-06 10:39:29 -0300

    Update reconhecimentoVoz.js

--- reconhecimentoVoz.js
+++ reconhecimentoVoz.js
@@ -1,13 +1,27 @@
-const SpeechRecognition =
-  window.SpeechRecognition || window.webkitSpeechRecognition;
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Adivinhe o número</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <main>
+        <h1>Acerte o número secreto</h1>
 
-const recognition = new SpeechRecognition();
-recognition.lang ='pt-br'
-recognition.start()
+        <h3>O número secreto está entre <span
+        id="menor-valor">0</span> e <span
+        id="maior-valor">100</span></h3>
 
-recognition.addEventListener("result", onspeak);
+        <div id"chute" class="mensagem">
+            <div>Você disse: </div>
+            <span class"box">10</span>
+            <div>O número secreto é maior </div>
+        </div>
+    </main>
 
-function onspeak(e)
-    {
-        console.log(e.result[0][0].transcript)
-    }
+    <script src="app/sortearNumero.js"></script>
+    <script src = "app/reconhecimentoVoz.js"></script>
+</body>
+</html>
