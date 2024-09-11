d58672b087b8e5e4427692b43dd14edb987f964e
SamuelVn007 <134400857+SamuelVn007@users.noreply.github.com>
2024-09-11 11:36:10 -0300

    Add files via upload

---
+++ app/reconhecimentoVoz.js
@@ -0,0 +1,22 @@
+const SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const Elementchute = document.getElementById("chute")
+const recognition = new SpeechRecognition();
+recognition.lang = "pt-br";
+recognition.start()
+
+recognition.addEventListener("result", onSpeak)
+recognition.addEventListener("end", () => recognition.start())
+function onSpeak(e){
+   chute = (e.results[0][0].transcript)
+   exibeChute(chute)
+   verificaChuteValido(chute)
+}
+
+function exibeChute(chute){
+  Elementchute.innerHTML = ` 
+  <div> Você disse:</div>
+  <span class="box">${chute}</span>
+  `
+}
---
+++ app/validacao.js
@@ -0,0 +1,28 @@
+    function verificaValorChuteFalso(chute){
+        const numero = +chute
+
+        if(chuteForInvalido(numero)){
+            Elementochute.innerHTML += `<div>Valor Inválido</div>`
+        }
+        if(numeroForMaiorOuMenorQueOValorPermitido(numero)){
+            Elementochute.innerHTML += `<div> Valor Inválido: O número secreto precisa estar entre ${menorValor} e ${maiorValor} </div>`
+        }
+        if( numero === numeroSecreto){
+            document.body.innerHTML = `
+            <h2> Você acertou!! </h2>
+            <h3> O número secreto era ${numeroSecreto} </h3>
+            `
+        }else if(numero > numeroSecreto) {
+         Elementochute.innerHTML += `<div> O número secreto é menor</div>`
+        } else{
+          Elementochute.innerHTML += `<div> O número secreto é maior </div>`  
+        }
+    }
+
+    function chuteForInvalido(numero){
+        return Number.isNaN(numero)
+    }
+
+    function numeroForMaiorOuMenorQueOValorPermitido(numero){
+        return numero > maiorValor || numero < menorValor
+    }
